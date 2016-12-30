a4454e2a7b0fc1f9061ac44d7b4640ab0fa04673
Leon Potgieter <hexvallei@gmail.com>
2016-12-30 17:38:24 +0000

    Initial Class Setup

--- todolist.rb
+++ todolist.rb
@@ -1,6 +1,33 @@
 # To-Do-List App using Classes and Methods
 
 =begin
-
+1) Add Items to List
+2) View List
+3) Clear List
 =end
+
+class MyList
+  attr_reader :list
+  attr_accessor :item
+    
+  def initialize(item)
+    @item = item
+    @list = []
+  end
+  
+  def add_item(item)
+    @list.push item
+  end
+  
+  def clear_list
+    @list = []
+  end
+  
+  def to_s
+    @list
+  end
+
+end
+
+
 
